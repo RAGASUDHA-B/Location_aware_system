8a4031f355218170ea24f79759d55985e6afd6cb
RAGASUDHA B <ragasudha@student.tce.edu>
2026-02-25 03:02:39 +0000

    updated ajax

--- lab_before_workout/jquery_ajax/samp1.js
+++ lab_before_workout/jquery_ajax/samp1.js
@@ -1,6 +1,10 @@
-$(document).ready(function(){
-    $('#btn').bind("click",function(){
-        $("input").blur();
-        $("p").html("this is blur method that is used!!!");
-    });
-});
+<html>
+    <head>
+        <title>jquery ajax() method</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    </head>
+    <body>
+        <h1>geeks for geeks</h1>
+        <h2>jquery ajax mathod</h2>
+    </body>
