d96c59904a239ffa2cf3b0fbea0041312041f0cb
RAGASUDHA B <ragasudha@student.tce.edu>
2026-02-25 05:42:06 +0000

    updated the code using jquery and ajax

--- location_project/login/script2.js
+++ location_project/login/script2.js
@@ -1,8 +1,44 @@
-document.getElementById("loginBtn").addEventListener("click", function () {
-    const inputs = document.querySelectorAll("input");
-    if (inputs[0].value === "" || inputs[1].value === "") {
-        alert("Please enter username and password");
-    } else {
-        alert("Login successful!");
-    }
+$(document).ready(function () {
+    $("input").blur(function () {
+        if ($(this).val() === "") {
+            $(this).css("background-color", "#ff1010");
+        } else {
+            $(this).css("background-color", "#e4ffba");
+        }
+    });
+    $("#loginBtn").click(function () {
+        let username = $("#username").val();
+        let password = $("#password").val();
+        if (username === "" || password === "") {
+            $(".login-box").animate({ left: "-10px" }, 50)
+        .animate({ left: "10px" }, 50)
+        .animate({ left: "-10px" }, 50)
+        .animate({ left: "10px" }, 50)
+        .animate({ left: "0px" }, 50);
+            $("#username").css("background-color", "#ff1010");
+            $("#password").css("background-color", "#ff1010");
+            alert("Please enter username and password");
+
+        } else {
+            $(".login-box").fadeTo(300, 0.5).fadeTo(300, 1);
+            //$(".input").css("background-color", "#e4ffba");
+            alert("Login successful!");
+        }
+        $ajax({
+            url:"#",
+            type:"POST",
+            data:{
+                username:username,
+                password:password
+            },
+            success:function(response){
+                if(username==="admin" && password==="password"){
+                    alert("Login successful!");
+                }else{
+                    alert("Invalid username or password");
+                }
+
+        });
+    });
+
 });
